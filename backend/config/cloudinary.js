3d2eba7516fd23d58ffc1528d08334e74d797c6c
Piyush6046 <badodepiyush@gmail.com>
2025-08-14 23:41:37 +0530

    set up multer, cloudinary and added profile page

---
+++ backend/config/cloudinary.js
@@ -0,0 +1,25 @@
+import {v2 as cloudinary} from 'cloudinary'
+import dotenv from 'dotenv'
+import fs from 'fs'
+dotenv.config()
+
+const uploadOnCloudinary = async (filePath) => {
+  cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET
+})
+
+try {
+  if(!filePath) return null
+  const uploadResult = await cloudinary.uploader.upload(filePath,{resource_type:"auto"})
+  fs.unlinkSync(filePath)
+  return uploadResult.secure_url
+
+} catch (error) {
+  console.log(error)
+  fs.unlinkSync(filePath)
+  return null
+}
+}
+export default uploadOnCloudinary
--- backend/controller/user.controller.js
+++ backend/controller/user.controller.js
@@ -21,3 +21,33 @@ export const getCurrentUser = async (req, res) => {
     });
   }
 }
+
+export const updateProfile= async(req,res)=>{
+  try {
+    const userId=req.userId
+    const{description,name}=req.body
+    let photoUrl;
+    if(req.file){
+      photoUrl= await uploadOnCloudinary(req.file.path)
+    }
+    const user= await User.findByIdAndUpdate(userId,{description,name,photoUrl},{new:true})
+    if(!user){
+      return res.status(400).json({
+        success:false,
+        message:"User not found"
+      })
+    }
+    res.status(200).json({
+      success:true,
+      message:"Profile updated successfully",
+      user
+    })
+
+  } catch (error) {
+    return res.status(500).json({
+      success:false,
+      message:"error while updating profile",
+      error
+    })
+  }
+}
