f49837cf1b17426b650d8f66cfcc5434efcbe4ae
Piyush6046 <badodepiyush@gmail.com>
2025-08-15 01:41:13 +0530

    added course controller

---
+++ backend/controller/course.controller.js
@@ -0,0 +1,166 @@
+import Course from "../model/course.Model"
+
+export const createCourse = async (req, res) => {
+  try {
+    const{tittle,category}=req.body
+    if(!tittle || !category){
+      return res.status(400).json({
+        success:false,
+        message:"All fields are required"
+      })
+    }
+    const course = await Course.create({
+      tittle,
+      description,
+      creator: req.userId,
+      category
+    })
+    return res.status(200).json({
+      success:true,
+      course
+    })
+  } catch (error) {
+    return res.status(500).json({
+      success:false,
+      message:"error while creating course",
+      error
+    })
+  }
+
+}
+
+export const getPublishedCourses = async (req, res) => {
+  try {
+    const courses = await Course.find({isPublished:true}).populate("creator","name photoUrl");
+    if(!courses){
+      return res.status(400).json({
+        success:false,
+        message:"Courses not found"
+      })
+    }
+    return res.status(200).json({
+      success:true,
+      courses
+    })
+  } catch (error) {
+    return res.status(500).json({
+      success:false,
+      message:"error while getting published courses",
+      error
+    })
+  }
+}
+
+export const getCreatorCourses=async (req, res) => {
+  try {
+    const userId=req.userId
+    const courses=await Course.find({creator:userId}).populate("creator","name photoUrl");
+    if(!courses){
+      return res.status(400).json({
+        success:false,
+        message:"Courses not found"
+      })
+    }
+    return res.status(200).json({
+      success:true,
+      courses
+    })
+  } catch (error) {
+    return res.status(500).json({
+      success:false,
+      message:"error while getting creator courses",
+      error
+    })
+  }
+}
+
+
+export const editCourse=async(req,res)=>{
+  try {
+    const {courseId}=req.params
+    const{tittle,subTitle,category,description,level,isPublished,price}=req.body
+    let thumbnail
+    if(req.file){
+      thumbnail=await uploadOnCloudinary(req.file.path)
+    }
+    const course=await Course.findById(courseId)
+    if(!course){
+      return res.status(400).json({
+        success:false,
+        message:"Course not found"
+      })
+    }
+    const updateData={
+      tittle,
+      subTitle,
+      category,
+      description,
+      level,
+      isPublished,
+      price,
+      thumbnail
+    }
+    course=await Course.findByIdAndUpdate(courseId,updateData,{new:true})
+    return res.status(200).json({
+      success:true,
+      course,
+      message:"Course edited successfully"
+    })
+  } catch (error) {
+    return res.status(500).json({
+      success:false,
+      message:"error while editing course",
+      error
+    })
+  }
+}
+
+export const getCourseById=async(req,res)=>{
+  try {
+    const {courseId}=req.params
+    const course=await Course.findById(courseId)
+    if(!course){
+      return res.status(400).json({
+        success:false,
+        message:"Course not found"
+      })
+    }
+    return res.status(200).json({
+      success:true,
+      course
+    })
+  } catch (error) {
+    return res.status(500).json({
+      success:false,
+      message:"error while getting course by id",
+      error
+    })
+  }
+}
+
+
+export const removeCourse=async(req,res)=>{
+  try {
+    const {courseId}=req.params
+    const course=await Course.findById(courseId)
+    if(!course){
+      return res.status(400).json({
+        success:false,
+        message:"Course not found"
+      })
+    }
+    course=await Course.findByIdAndDelete(courseId)
+    return res.status(200).json({
+      success:true,
+      message:"Course deleted successfully"
+    })
+  } catch (error) {
+    return res.status(500).json({
+      success:false,
+      message:"error while deleting course",
+      error
+    })
+  }
+}
+
+
